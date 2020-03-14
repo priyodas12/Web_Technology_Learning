136a902cc0628871605094cf694b36afbf7dc478
Priyobrato Das <priyodas12@gmail.com>
2020-03-14 22:23:13 +0530

    function tricky 1

--- ES6/Function.js
+++ ES6/Function.js
@@ -5,13 +5,13 @@ function fn(){
 }
 
 fn();
-
+//es6 way
 let func=()=>{
   console.log("\nThis is ES6 function syntax!\n ");
 }
 
 func();
-
+//with return type
 let constSum=()=>{
   const a=20;
   const j=3;
@@ -19,3 +19,18 @@ let constSum=()=>{
 }
 
 console.log(constSum());
+
+//with arguments
+let sum=(m,n)=>{
+  return m+n;
+}
+
+console.log("Sum :"+sum(23,78));
+
+
+//tricky 1
+let sum2=m=>{
+  return m+10;
+}
+
+console.log("Sum :"+sum2(23,78));
