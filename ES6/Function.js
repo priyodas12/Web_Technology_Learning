769be2db337d9193eab7c0294135c1374bfe00ba
Priyobrato Das <priyodas12@gmail.com>
2020-03-14 22:14:13 +0530

    function es6 added

--- ES6/Function.js
+++ ES6/Function.js
@@ -1,7 +1,13 @@
 //traditional way or ES5
 
 function fn(){
-  console.log("\nthis is ES5 funtion syntax!\n");
+  console.log("\nthis is ES5 function syntax!\n");
 }
 
 fn();
+
+let func=()=>{
+  console.log("\nThis is ES6 function syntax!\n ");
+}
+
+func();
