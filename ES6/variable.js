bf2b6788d209a27d1242cf08e5933677d3a19d56
Priyobrato Das <priyodas12@gmail.com>
2020-03-14 21:58:42 +0530

    let and const name changed

--- ES6/variable.js
+++ ES6/variable.js
@@ -2,7 +2,7 @@ if(true){
   let a=10;
 }//let having block scope
 let a=24;
-const b=20;
+const B=20;
 a=30;
 //b=50; //TypeError: Assignment to constant variable
-console.log(a*b);
+console.log(a*B);
