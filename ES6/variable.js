8be321c6da51e32782e69acb630005d06e6d60be
Priyobrato Das <priyodas12@gmail.com>
2020-03-14 22:02:52 +0530

    let and const array

--- ES6/variable.js
+++ ES6/variable.js
@@ -6,3 +6,10 @@ const B=20;
 a=30;
 //b=50; //TypeError: Assignment to constant variable
 console.log(a*B);
+
+//this ROADS will be pointing to the object location.
+const ROADS=["3A","4G","6H"];
+console.log(ROADS);
+//pointer can not been changed but the values can be altered.
+ROADS.push("8J");
+console.log(ROADS);
