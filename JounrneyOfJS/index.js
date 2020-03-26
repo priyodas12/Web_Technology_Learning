a959447a226a28b7708b8b26775d566ad99bf33d
Priyobrato Das <priyodas12@gmail.com>
2020-03-26 14:02:46 +0530

    added tweeter count

--- JounrneyOfJS/index.js
+++ JounrneyOfJS/index.js
@@ -1,1 +1,3 @@
-alert('Hello World!');
+window.alert('Hello World!');
+window.alert('again');
+window.alert('again');
---
+++ JounrneyOfJS/tweetLengthCount.js
@@ -0,0 +1,4 @@
+var a="abc";
+
+console.log("You have typed "+a.length+" charactes, left "+ (280-a.length)+" Characters !")
+//prompt("You have typed "+a.length+" charactes, left "+ (280-a.length)+" Characters !);
