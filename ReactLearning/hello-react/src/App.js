129a34fbf3faa3f53cf4b279a1e367bd39d16528
Priyobrato Das <priyodas12@gmail.com>
2020-04-01 15:31:33 +0530

    added class component

--- ReactLearning/hello-react/src/App.js
+++ ReactLearning/hello-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet'
+import Welcome from './Components/GreetClass'
 
 function App() {
   return (
     <div className="App">
         <Greet />
+        <Welcome />
     </div>
   );
 }
